e568da7f00b12a9c9eb07d0349a5a861a9603946
Ben Parker <bpar164@aucklanduni.ac.nz>
2020-08-13 17:27:33 +1200

    Started on exam page.

--- code/src/exams/exams.controller.ts
+++ code/src/exams/exams.controller.ts
@@ -2,28 +2,31 @@ import { Controller, Get, Render, Request, UseFilters, UseGuards } from '@nestjs
 import { ExamsService } from './exams.service';
 import { AuthExceptionFilter } from 'src/user/auth-exceptions.filter';
 import { AuthenticatedGuard } from 'src/user/authenticated.guard';
+import { UserService } from 'src/user/user.service';
 
 @Controller()
 @UseFilters(AuthExceptionFilter)
 
 export class ExamsController {
-    constructor(private readonly examsService: ExamsService) {}
+    constructor(private readonly examsService: ExamsService, private readonly userService: UserService) {}
     @UseGuards(AuthenticatedGuard)
     @Get('exams')
     @Render('exams')
-    getAdvancedView(@Request() req) { 
+    async getExamsView(@Request() req) { 
+        let userID = await this.userService.getUserIDByEmail(req.user.email);
         return { 
             title: 'Exams', 
             description: 'Create exams using your multiple-choice and advanced questions',
             loggedIn: (req.user !== undefined) ? true : false, 
-            picture: req.user ? req.user.picture : null 
+            picture: req.user ? req.user.picture : null,
+            id: userID 
           };
     }
 
     @UseGuards(AuthenticatedGuard)
     @Get('exams-my')
     @Render('exams-my')
-    getAdvancedMyView(@Request() req) { 
+    getExamsMyView(@Request() req) { 
         return { 
             title: 'Exams', 
             description: 'Browse the exams that you have created',
--- code/src/exams/exams.module.ts
+++ code/src/exams/exams.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ExamsController } from './exams.controller';
 import { ExamsService } from './exams.service';
+import { UserService } from 'src/user/user.service';
+import { UserSchema, User } from 'src/user/user.schema';
 
 @Module({
+  imports: [
+     
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [ExamsController],
-  providers: [ExamsService]
+  providers: [ExamsService, UserService]
 })
 export class ExamsModule {}
